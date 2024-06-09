ec569b1af8e2b6e3d81e14152e3e14c574d95aba
albertolunia <albertolunia@gmail.com>
2024-06-09 19:37:08 -0300

    Trocando nome da pasta e adicionando o desafio 2.9

---
+++ 2.9.Desafio_variaveis_e_operadores_aritmeticos/CalculaMedia.java
@@ -0,0 +1,13 @@
+public class CalculaMedia {
+    public static void main(String[] args) {
+        double valor1, valor2, valor3, CalcularMedia;
+        
+        valor1 = 20;
+        valor2 = 30;
+        valor3 = 100;
+
+        CalcularMedia = (valor1 + valor2 + valor3) / 3;
+
+        System.out.println("A média dos valores é: " + CalcularMedia);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Java-AlgaWorks-JavaBasico
+
+Repositorio destinado ao treino de praticas basicas de java do curso da Alga Works
